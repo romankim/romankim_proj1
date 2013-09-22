c35914761bc05fc8800d80d95eeba9ebd6555668
romankim <romankim@mit.edu>
2013-09-22 01:35:22 -0400

    implement DOM version

--- site/public/js/main_game.js
+++ site/public/js/main_game.js
@@ -1,5 +1,4 @@
 
-// a demonstration program using the graphics library
 (function () {
     
 	// define some colors
@@ -9,15 +8,51 @@
 	var blue = Color(0,0,255);
     
 	// create the drawing pad object and associate with the canvas
+    // the graphical width and height is set in the style of the canvas element
 	pad = Pad(document.getElementById('canvas'));
     
-    var X_SIZE = 40;
-    var Y_SIZE = 40;
+    
+    // define the number of cells in x-axis and y-axis
+    var X_SIZE = 80;
+    var Y_SIZE = 80;
     
     board1 = Board(X_SIZE, Y_SIZE,start_state=1);
+    
+    // the intermediary variable from board data to board graphics
     var GRIDSIZE_X = pad.get_width() / X_SIZE;
     var GRIDSIZE_Y = pad.get_height() / Y_SIZE;
     
+    
+    
+    //initialize DOM-based pane
+    
+    cells = [];
+    for(var i = 0; i < X_SIZE; i++) {
+        
+        cells[i] = [];
+        
+        for(var j = 0; j < Y_SIZE; j++) {
+            var cell = document.createElement('div');
+            $(cell).addClass("cell")
+                   .css("height",""+GRIDSIZE_Y)
+                   .css("width",""+GRIDSIZE_X)
+                   .css("left",""+i*GRIDSIZE_X)
+                   .css("top",""+j*GRIDSIZE_Y)
+                   .appendTo($("#canvas2"));    
+            cells[i][j] = cell;
+            
+            if(board1.grid_array[i][j]===1) {
+                $(cell).addClass("alive");
+            }
+            else {
+                $(cell).addClass("dead");
+            }
+        }
+    }   
+    
+    
+    
+    // graphics paint function
     paint = function() {
         pad.clear();
        
@@ -28,16 +63,34 @@
                     GRIDSIZE_Y, 1, green,green);
                 }
             }
-        }    
+        }
         
         pad.draw_rectangle(Coord(0,0), pad.get_width(), pad.get_height(), 5, black);
     }
     
+    // DOM paint function
+    paint_dom = function() {
+        for(var i = 0; i < X_SIZE; i++) {
+            for(var j = 0; j < Y_SIZE; j++) {
+                $(cells[i][j]).removeClass("alive");
+                $(cells[i][j]).removeClass("dead");
+                if(board1.grid_array[i][j]===1) {
+                    $(cells[i][j]).addClass("alive");
+                }
+                else {
+                    $(cells[i][j]).addClass("dead");
+                }
+            }
+        }
+    }
+    
     paint();
+    paint_dom();
     
     var stepFunction = function() {
         board1.step();
         paint();
+        paint_dom();
     }
     
     looping = false;
